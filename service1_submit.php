61a5aebffbfcdd8eaaa7b55030c8a416da8af9b4
Rogith-Ranger <rogithy@gmail.com>
2020-07-25 21:38:12 +0530

    service 1

---
+++ service1_submit.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "xy";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO service1 VALUES ('John', 'Doe', 'john@example.com','asd')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
